5f208095126641d5d3568b68746414bb544f837c
keshav-valgen <keshav@valgen.com>
2017-02-17 09:05:21 +0530

    final

--- R/segment.R
+++ R/segment.R
@@ -8,14 +8,9 @@
 
 segment <- function(access_token, instance_url, object, field){
 
-#inputs <- c(data)
-# mytext <- c(inputs[['access_token']], inputs[['instance_url']],
-#             '36.0', inputs[['object']], inputs[['field']])
-# access_t <- inputs[1]
+
 instance_u <- paste0(instance_url,'/')
 api <- '36.0'
-# obj <- inputs[3]
-# field <- inputs[4]
 
 myquery <- paste0('Select Id, ', field,' FROM ', object)
 session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
