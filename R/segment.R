c645f6f217c1bbeef95db54ae43c1ee0d5f1b31e
keshav-valgen <keshav@valgen.com>
2017-05-05 12:46:36 +0530

    new

--- R/segment.R
+++ R/segment.R
@@ -24,5 +24,13 @@ data2 <- slider(data2, 5)
 data1 <- cbind(data1, data2) # Derived values are binded to the original data
 data1 <- subset(data1, select = c("Id", "dist"))
 colnames(data1) <- c("strId", "dist")
-return(data1)
+update_job <- rforcecom.createBulkJob(session,
+                                      operation ='update', object = object) # Create a new bulkjob for updating Salesforce
+my_data <- data.frame(id = data1$strId, v2 = data1$dist) # Dataframe to be uploaded
+colnames(my_data) <- c("id", newname)
+
+batches_info <- rforcecom.createBulkBatch(session,
+                                          jobId = update_job$id, data = my_data) #Update job
+
+return(nrow(data1))
 }
