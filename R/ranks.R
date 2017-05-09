77bf5a85110d3eaac47c4dde95970354f8dd5f7f
keshav-valgen <keshav@valgen.com>
2017-05-09 11:55:50 +0530

    Updated Ranking mechanism

--- R/ranks.R
+++ R/ranks.R
@@ -16,11 +16,9 @@ ranks <- function(access_token, instance_url, object, depfield, indfield){
   data1 <- ranker(data1)
   data1 <- subset(data1, select = c("Id", "decile"))
   colnames(data1) <- c("strId", "dist")
-  data1$dist[data1$dist == 1 | data1$dist == 2] <- 1
-  data1$dist[data1$dist == 3 | data1$dist == 4] <- 2
-  data1$dist[data1$dist == 5 | data1$dist == 6] <- 3
-  data1$dist[data1$dist == 7 | data1$dist == 8] <- 4
-  data1$dist[data1$dist == 9 | data1$dist == 10] <- 5
-  data1$dist[data1$dist == 11] <- 6
+  
+  data1$dist[data1$dist == 5 | data1$dist == 6 | data1$dist == 7 | data1$dist == 8] <- 5
+  data1$dist[data1$dist == 9 | data1$dist == 10 | data1$dist == 11] <- 6
+ 
   return(data1)
 }
