a8b44078ba9dcbdd33bfcfffa45dceaac424b7e8
sudhakar-valgen <sudhakar.b@valgen.com>
2019-05-22 17:51:33 +0530

    updater function is created

--- R/functions.R
+++ R/functions.R
@@ -8,6 +8,7 @@
 #'@export ranker
 #'@export index
 #'@export slider2
+#'@export updater
 
 
 
@@ -185,6 +186,21 @@ slider2 <- function(var, iter) # 2 inputs; The numeric variable to convert and N
     return(fdata)
     # Upto 5 cuts can be done
   }
+}
+
+updater <- function(access_token, instance_url, myobject, data){
+  
+  instance_u <- paste0(instance_url,'/')
+  api <- '36.0'
+  session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
+  
+  update_job <- rforcecom.createBulkJob(session,
+                                        operation ='update', object = myobject) # Create a new bulkjob for updating Salesforce
+  my_data <- data # Dataframe to be uploaded
+  batches_info <- rforcecom.createBulkBatch(session,
+                                            jobId = update_job$id, data = my_data) #Update job
+  close_job_info <- rforcecom.closeBulkJob(session, jobId=update_job$id)
+  
 }
 
 
